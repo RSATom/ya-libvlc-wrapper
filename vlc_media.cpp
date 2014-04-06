197f4f641a3a8eb5e25fca38d760ddd555984738
Sergey Radionov <RSATom@gmail.com>
2014-04-06 11:28:42 +0700

    vlc::media class

---
+++ vlc_media.cpp
@@ -0,0 +1,116 @@
+/*****************************************************************************
+* Copyright (c) 2013-2014, Sergey Radionov <rsatom_gmail.com>
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the project nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*****************************************************************************/
+
+#include "vlc_media.h"
+
+using namespace vlc;
+
+media::media()
+    : m_media( 0 )
+{
+}
+
+media::media( ::libvlc_media_t* m, bool needs_retain )
+    : m_media( m )
+{
+    if( m_media && needs_retain )
+        libvlc_media_retain( m_media );
+}
+
+media::media( const media& other )
+    : m_media( other.m_media )
+{
+    if( m_media )
+        libvlc_media_retain( m_media );
+}
+
+media::~media()
+{
+    release_media();
+}
+
+void media::release_media()
+{
+    if( m_media ) {
+        libvlc_media_release( m_media );
+        m_media = 0;
+    }
+}
+
+media& media::operator= ( const media& m )
+{
+    release_media();
+
+    m_media = m.m_media;
+    if( m_media )
+        libvlc_media_retain( m_media );
+
+    return *this;
+}
+
+std::string media::mrl() const
+{
+    std::string ret;
+
+    if( !m_media )
+        return ret;
+
+    if( char* mrl = libvlc_media_get_mrl( m_media ) ) {
+        ret = mrl;
+        libvlc_free( mrl );
+    }
+
+    return ret;
+}
+
+std::string media::meta( libvlc_meta_t meta_id ) const
+{
+    std::string ret;
+
+    if( m_media ) {
+        if( char* meta = libvlc_media_get_meta( m_media, meta_id ) ) {
+            ret = meta;
+            libvlc_free( meta );
+        }
+    }
+
+    return ret;
+}
+
+bool media::is_parsed() const
+{
+    if( m_media )
+       return libvlc_media_is_parsed( m_media ) != 0;
+
+    return false;
+}
+
+void media::parse()
+{
+    if( m_media )
+        libvlc_media_parse( m_media );
+}
---
+++ vlc_media.h
@@ -0,0 +1,70 @@
+/*****************************************************************************
+* Copyright (c) 2013-2014, Sergey Radionov <rsatom_gmail.com>
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the project nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*****************************************************************************/
+
+#pragma once
+
+#include <vlc/vlc.h>
+
+namespace vlc
+{
+    class media
+    {
+    public:
+        media();
+        explicit media( libvlc_media_t*, bool needs_retain );
+        media( const media& other );
+        ~media();
+
+        media& operator= ( const media& m );
+
+        bool operator== ( const media& m ) const
+            { return m_media == m.m_media; }
+        bool operator!= ( const media& m ) const
+            { return m_media != m.m_media; }
+        bool operator== ( libvlc_media_t* m ) const
+            { return m_media == m; }
+        bool operator!= ( libvlc_media_t* m ) const
+            { return m_media != m; }
+        operator bool() const
+            { return m_media != 0; }
+
+        ::libvlc_media_t* libvlc_media_t() const
+            { return m_media; }
+
+        bool is_parsed() const;
+        void parse();
+
+        std::string mrl() const;
+        std::string meta( libvlc_meta_t meta_id ) const;
+
+    private:
+        void release_media();
+
+    private:
+        ::libvlc_media_t* m_media;
+    };
+};
